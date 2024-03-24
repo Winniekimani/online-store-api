33e63e060e4d74cb415ff489888c73bf8cd2bd0c
Winniekimani <kimaniwinniew@gmail.com>
2024-03-24 16:00:39 +0300

    Add config server for centralized configuration management

---
+++ online-shopping-config-server/src/test/java/com/example/onlineshoppingconfigserver/OnlineShoppingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshoppingconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
